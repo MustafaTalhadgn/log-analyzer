427e10270b353f5d281919e90b64bd00b3b43215
Mustafa Talha DOĞAN <dgn.mustafatalha@gmail.com>
2026-02-10 16:25:46 +0300

    entities layer katmanındaki alert.go ve rule.go  güncellendi gorm kütüphanesi eklendi .

--- Backend/internal/entities/rule.go
+++ Backend/internal/entities/rule.go
@@ -1,25 +1,38 @@
 package entities
 
+import (
+	"gorm.io/gorm"
+)
+
 type Rule struct {
-	Id           string    `json:"id" yaml:"id"`
-	Description  string    `json:"description" yaml:"description"`
-	LogType      string    `json:"log_type" yaml:"log_type"`
-	Match        Match     `json:"match" yaml:"match"`
-	Extract      Extract   `json:"extract" yaml:"extract"`
-	Threshold    Threshold `json:"threshold" yaml:"threshold"`
-	Severity     string    `json:"severity" yaml:"severity"`
-	AlertMessage string    `json:"alert_message" yaml:"alert_message"`
+	gorm.Model
+	RuleId       string `json:"rule_id" yaml:"id" gorm:"uniqueIndex;not null"`
+	Description  string `json:"description" yaml:"description"`
+	LogType      string `json:"log_type" yaml:"log_type"`
+	Severity     string `json:"severity" yaml:"severity"`
+	AlertMessage string `json:"alert_message" yaml:"alert_message"`
+
+	// gorm embedded yaptık
+	Match     Match     `json:"match" yaml:"match" gorm:"embedded;embeddedPrefix:match_"`
+	Extract   Extract   `json:"extract" yaml:"extract" gorm:"embedded;embeddedPrefix:extract_"`
+	Threshold Threshold `json:"threshold" yaml:"threshold" gorm:"embedded;embeddedPrefix:threshold_"`
 }
 
 type Match struct {
 	Type  string `json:"type" yaml:"type"`
 	Value string `json:"value" yaml:"value"`
 }
+
 type Extract struct {
 	IpRegex   string `json:"ip_regex" yaml:"ip_regex"`
 	UserRegex string `json:"user_regex" yaml:"user_regex"`
 }
+
 type Threshold struct {
 	Count         int `json:"count" yaml:"count"`
 	WithinSeconds int `json:"within_seconds" yaml:"within_seconds"`
+}
+
+func (Rule) TableName() string {
+	return "rules"
 }
